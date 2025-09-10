78bb5cb8201f7f6707da1f619a6ea22f7e3d5a0d
Christian <christian.darosa0106@gmail.com>
2025-09-10 19:02:03 -0300

    dale gremio

--- auth.js
+++ auth.js
@@ -4,8 +4,8 @@ console.log("chamando API")
 export default function autenticador(req, res, next) {
   const { tipoUsuario } = req.body;
 
-  if (tipoUsuario && tipoUsuario === "adm") {
-    return next(); // autorizado
+  if (tipoUsuario && tipoUsuario === "ADM") {
+    return next();
   }
 
   return res.status(401).json({ mensagem: "Funcao nao permitida para esse usuario" });
